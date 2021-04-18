8d7c2974f671b115daf9b06d516340deadddd806
Muhammad <shafiquecbl@gmail.com>
2021-04-18 21:12:33 +0500

    Dashboard Updated

--- lib/screens/Home_Screen/griddashboard.dart
+++ lib/screens/Home_Screen/griddashboard.dart
@@ -15,9 +15,9 @@ class GridDashboard extends StatelessWidget {
   Widget build(BuildContext context) {
     return StaggeredGridView.count(
       crossAxisCount: 2,
-      crossAxisSpacing: 30,
-      mainAxisSpacing: 30,
-      padding: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+      crossAxisSpacing: 25,
+      mainAxisSpacing: 25,
+      padding: EdgeInsets.symmetric(horizontal: 5, vertical: 20),
       staggeredTiles: [
         StaggeredTile.extent(1, 130),
         StaggeredTile.extent(1, 130),
@@ -36,7 +36,7 @@ class GridDashboard extends StatelessWidget {
       onTap: () {
         navigator(context, ManageUsers());
       },
-      splashColor: Color(0xFFFACE13),
+      splashColor: Color(0xFF8D4DE9),
       child: Container(
         decoration: BoxDecoration(boxShadow: [
           BoxShadow(
@@ -45,7 +45,7 @@ class GridDashboard extends StatelessWidget {
             blurRadius: 2,
             offset: Offset(1, 0),
           )
-        ], color: Color(0xFFFACE13), borderRadius: BorderRadius.circular(10)),
+        ], color: Colors.greenAccent, borderRadius: BorderRadius.circular(10)),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
@@ -82,7 +82,7 @@ class GridDashboard extends StatelessWidget {
             blurRadius: 2,
             offset: Offset(1, 0),
           )
-        ], color: Color(0xFF17D577), borderRadius: BorderRadius.circular(10)),
+        ], color: Color(0xFF8D4DE9), borderRadius: BorderRadius.circular(10)),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
