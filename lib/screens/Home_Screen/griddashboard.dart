3099a28d87ae6799e305136f8373df408e5638c5
Muhammad <shafiquecbl@gmail.com>
2021-04-22 16:30:29 +0500

    Inbox Resolved

--- lib/screens/Home_Screen/griddashboard.dart
+++ lib/screens/Home_Screen/griddashboard.dart
@@ -146,7 +146,7 @@ class GridDashboard extends StatelessWidget {
                 ),
                 StreamBuilder(
                   stream: FirebaseFirestore.instance
-                      .collection('Users')
+                      .collection('Admin')
                       .doc(FirebaseAuth.instance.currentUser.email)
                       .collection('Contact US')
                       .where('Status', isEqualTo: 'unread')
@@ -154,6 +154,7 @@ class GridDashboard extends StatelessWidget {
                   builder: (BuildContext context, AsyncSnapshot snapshot) {
                     if (snapshot.connectionState == ConnectionState.waiting)
                       return Container();
+                    if (snapshot.data.docs.length == 0) return Container();
                     return Container(
                       padding: EdgeInsets.all(2),
                       decoration: new BoxDecoration(
